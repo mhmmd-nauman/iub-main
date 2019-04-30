de33b520c62b8ef20b43af991d50a25724b7c9f1
Web <Web@Hi>
2019-04-30 15:59:22 +0500

    test icons and grids

--- index.php
+++ index.php
@@ -136,6 +136,8 @@ jQuery(window).on('load',  function() {
                                     <li><a href = "#">Vision Statement</a></li>
                                     <li class = "divider"></li>
                                     <li><a href = "#">Research Center</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Contact Directory</a></li>
                                  </ul>
 
                               </li>
@@ -200,14 +202,94 @@ jQuery(window).on('load',  function() {
 
                                  <ul class = "dropdown-menu">
                                     <li><a href = "#">Affiliated Institutes</a></li>
-                                    
+                                    <li class = "divider"></li>
                                     <li><a href = "#">Divisions</a></li>
-                                    
+                                    <li class = "divider"></li>
                                     <li><a href = "#">Faculties</a></li> 
-                                    
+                                    <li class = "divider"></li>
                                     <li><a href = "#">Institutes</a></li> 
                                    
                                  </ul>
+
+                              </li>
+                              <li class = "dropdown">
+                                 <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">
+                                    Admissions
+                                    <b class = "caret"></b>
+                                 </a>
+
+                                 <ul class = "dropdown-menu">
+                                    <li><a href = "#">Admission Portal</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Academic Calendar</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Courses and Programs</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Undergraduate Admissions</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Postgraduate Admissions</a></li> 
+                                   
+                                 </ul>
+
+                              </li>
+                              <li class = "dropdown">
+                                 <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">
+                                    Research
+                                    <b class = "caret"></b>
+                                 </a>
+
+                                 <ul class = "dropdown-menu">
+                                    <li><a href = "#">Office of Research & Technology</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Research Institutes</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#"></a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#"></a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#"></a></li> 
+                                   
+                                 </ul>
+
+                              </li>
+                              <li class = "dropdown">
+                                 <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">
+                                    Campus Life
+                                    <b class = "caret"></b>
+                                 </a>
+
+                                 <ul class = "dropdown-menu">
+                                    <li><a href = "#">Campus Life</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Art & Culture</a></li>
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Sports @ IUB</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Campus Facilities</a></li> 
+                                    <li class = "divider"></li>                                    
+                                    <li><a href = "#">Campus Accomodation</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Campus Resources</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Transportation</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Student Activities</a></li> 
+                                    <li class = "divider"></li>
+                                    <li><a href = "#">Student Support</a></li> 
+                                   
+                                 </ul>
+
+                              </li>
+                              <li>
+                                 <a href = "#" >
+                                    Careers@IUB
+                                 </a>
+
+                              </li>
+                              <li>
+                                 <a href = "#" >
+                                    IUB Email
+                                 </a>
 
                               </li>
 
